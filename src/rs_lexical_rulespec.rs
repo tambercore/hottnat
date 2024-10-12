e86bda5a9154bfcc8e2296ee3f39860d2e3ca94b
aswarbs <amberswarbrick1@gmail.com>
2024-10-12 20:05:03 +0100

    add: lexical rules and tests for has_char and f_has_char

--- src/rs_lexical_rulespec.rs
+++ src/rs_lexical_rulespec.rs
@@ -18,7 +18,22 @@ pub fn f_has_suffix(sentence: Vec<(&str, Wordclass)>, current_index: i32, suffix
     }
 }
 
+/// Function to check if the word at `current_index` contains char `c` and is not yet tagged.
+pub fn has_char(sentence: Vec<(&str, Wordclass)>, current_index: i32, c: char) -> bool {
+    match sentence.get(current_index as usize) {
+        Some(&(word, Wordclass::ANY)) => word.contains(c),
+        _ => false,
+    }
+}
 
+/// Function to check if the word at `current_index` contains char `c` and is tagged.
+pub fn f_has_char(sentence: Vec<(&str, Wordclass)>, current_index: i32, c: char) -> bool {
+    match sentence.get(current_index as usize) {
+        Some(&(word, Wordclass::ANY)) => false,
+        Some(&(word, _)) => word.contains(c),
+        _ => false,
+    }
+}
 
 #[test]
 fn test_has_suffix_found() {
@@ -72,5 +87,58 @@ fn test_has_f_suffix_not_found() {
     ];
     assert!(!f_has_suffix(sentence.clone(), 1, "ick"));
     assert!(!f_has_suffix(sentence.clone(), 2, "abcd"));
+
+}
+
+#[test]
+fn test_has_char_found() {
+    let sentence = vec![
+        ("The", Wordclass::ANY),
+        ("quick", Wordclass::ANY),
+        ("brown", Wordclass::ANY),
+        ("fox", Wordclass::ANY),
+    ];
+    assert!(has_char(sentence.clone(), 1, 'q'));
+    assert!(has_char(sentence.clone(), 2, 'n'));
+
+}
+
+#[test]
+fn test_has_char_not_found() {
+    let sentence = vec![
+        ("The", Wordclass::ANY),
+        ("quick", Wordclass::JJ),
+        ("brown", Wordclass::ANY),
+        ("fox", Wordclass::ANY),
+    ];
+    assert!(!has_char(sentence.clone(), 1, 'q'));
+    assert!(!has_char(sentence.clone(), 2, 'k'));
+
+}
+
+
+#[test]
+fn test_f_has_char_found() {
+    let sentence = vec![
+        ("The", Wordclass::ANY),
+        ("quick", Wordclass::JJ),
+        ("brown", Wordclass::JJ),
+        ("fox", Wordclass::ANY),
+    ];
+    assert!(f_has_char(sentence.clone(), 1, 'q'));
+    assert!(f_has_char(sentence.clone(), 2, 'n'));
+
+}
+
+#[test]
+fn test_f_has_char_not_found() {
+    let sentence = vec![
+        ("The", Wordclass::ANY),
+        ("quick", Wordclass::ANY),
+        ("brown", Wordclass::JJ),
+        ("fox", Wordclass::ANY),
+    ];
+    assert!(!f_has_char(sentence.clone(), 1, 'q'));
+    assert!(!f_has_char(sentence.clone(), 2, 'k'));
 
 }
