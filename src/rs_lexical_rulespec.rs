4e3793b76c0f27e8c166b58b9261fb6a23d2ee26
aswarbs <amberswarbrick1@gmail.com>
2024-10-12 20:48:31 +0100

    add: lexical rules delete_prefix and f_delete_prefix and their tests

--- src/rs_lexical_rulespec.rs
+++ src/rs_lexical_rulespec.rs
@@ -58,12 +58,189 @@ pub fn f_add_suffix(sentence: Vec<(&str, Wordclass)>, current_index: i32, suffix
     }
 }
 
+/// Function to check if the word at `current_index` is still a word if `suffix` is deleted, and is not yet tagged.
+pub fn delete_suffix(sentence: Vec<(&str, Wordclass)>, current_index: i32, suffix: &str, wc_mapping: &WordclassMap) -> bool {
+    match sentence.get(current_index as usize) {
+        Some(&(word, Wordclass::ANY)) => {
+            match word.strip_suffix(suffix) {
+                Some(modified_word) => is_word_in_lexicon(modified_word, wc_mapping),
+                _ => false
+            }
+        },
+        _ => false,
+    }
+}
+
+/// Function to check if the word at `current_index` is still a word if `suffix` is deleted, and is tagged.
+pub fn f_delete_suffix(sentence: Vec<(&str, Wordclass)>, current_index: i32, suffix: &str, wc_mapping: &WordclassMap) -> bool {
+    match sentence.get(current_index as usize) {
+        Some(&(_, Wordclass::ANY)) => false,
+        Some(&(word, _)) => {
+            match word.strip_suffix(suffix) {
+                Some(modified_word) => is_word_in_lexicon(modified_word, wc_mapping),
+                _ => false
+            }
+        },
+        _ => false,
+    }
+}
+
+
+/// Function to check if the word at `current_index` is still a word if `prefix` is deleted, and is not yet tagged.
+pub fn delete_prefix(sentence: Vec<(&str, Wordclass)>, current_index: i32, prefix: &str, wc_mapping: &WordclassMap) -> bool {
+    match sentence.get(current_index as usize) {
+        Some(&(word, Wordclass::ANY)) => {
+            match word.strip_prefix(prefix) {
+                Some(modified_word) => is_word_in_lexicon(modified_word, wc_mapping),
+                _ => false
+            }
+        },
+        _ => false,
+    }
+}
+
+/// Function to check if the word at `current_index` is still a word if `prefix` is deleted, and is tagged.
+pub fn f_delete_prefix(sentence: Vec<(&str, Wordclass)>, current_index: i32, prefix: &str, wc_mapping: &WordclassMap) -> bool {
+    match sentence.get(current_index as usize) {
+        Some(&(_, Wordclass::ANY)) => false,
+        Some(&(word, _)) => {
+            match word.strip_prefix(prefix) {
+                Some(modified_word) => is_word_in_lexicon(modified_word, wc_mapping),
+                _ => false
+            }
+        },
+        _ => false,
+    }
+}
+
+
 /// Function to check if `word` appears in the Wordclass mappings retrieved from the lexicon.
 pub fn is_word_in_lexicon(word: &str, wc_mapping: &WordclassMap) -> bool {
     match wc_mapping.get(word) {
         Some(_) => true,
         _ => false
     }
+}
+
+
+#[test]
+fn test_delete_suffix_found() {
+    let wc_mapping: WordclassMap = initialize_tagger("data/lexicon.txt").unwrap();
+    let sentence = vec![
+        ("The", Wordclass::ANY),
+        ("quickest", Wordclass::ANY),
+        ("brown", Wordclass::ANY),
+        ("lazy", Wordclass::ANY),
+        ("dog", Wordclass::ANY),
+    ];
+    assert!(delete_suffix(sentence.clone(), 1, "est", &wc_mapping));
+    assert!(delete_suffix(sentence.clone(), 2, "n", &wc_mapping));
+
+}
+
+#[test]
+fn test_delete_suffix_not_found() {
+    let wc_mapping: WordclassMap = initialize_tagger("data/lexicon.txt").unwrap();
+    let sentence = vec![
+        ("The", Wordclass::ANY),
+        ("quickest", Wordclass::JJ),
+        ("brown", Wordclass::ANY),
+        ("lazy", Wordclass::ANY),
+        ("dog", Wordclass::ANY),
+    ];
+    assert!(!delete_suffix(sentence.clone(), 1, "est", &wc_mapping));
+    assert!(!delete_suffix(sentence.clone(), 2, "own", &wc_mapping));
+
+}
+
+
+#[test]
+fn test_delete_f_suffix_found() {
+    let wc_mapping: WordclassMap = initialize_tagger("data/lexicon.txt").unwrap();
+    let sentence = vec![
+        ("The", Wordclass::DT),
+        ("quickest", Wordclass::JJ),
+        ("brown", Wordclass::JJ),
+        ("fox", Wordclass::NN),
+    ];
+    assert!(f_delete_suffix(sentence.clone(), 1, "est", &wc_mapping));
+    assert!(f_delete_suffix(sentence.clone(), 2, "n", &wc_mapping));
+
+}
+
+#[test]
+fn test_delete_f_suffix_not_found() {
+    let wc_mapping: WordclassMap = initialize_tagger("data/lexicon.txt").unwrap();
+    let sentence = vec![
+        ("The", Wordclass::DT),
+        ("quickest", Wordclass::ANY),
+        ("brown", Wordclass::JJ),
+        ("fox", Wordclass::NN),
+    ];
+    assert!(!f_delete_suffix(sentence.clone(), 1, "est", &wc_mapping));
+    assert!(!f_delete_suffix(sentence.clone(), 2, "own", &wc_mapping));
+
+}
+
+#[test]
+fn test_delete_prefix_found() {
+    let wc_mapping: WordclassMap = initialize_tagger("data/lexicon.txt").unwrap();
+    let sentence = vec![
+        ("The", Wordclass::ANY),
+        ("unquick", Wordclass::ANY),
+        ("unbrown", Wordclass::ANY),
+        ("lazy", Wordclass::ANY),
+        ("dog", Wordclass::ANY),
+    ];
+    assert!(delete_prefix(sentence.clone(), 1, "un", &wc_mapping));
+    assert!(delete_prefix(sentence.clone(), 2, "un", &wc_mapping));
+
+}
+
+#[test]
+fn test_delete_prefix_not_found() {
+    let wc_mapping: WordclassMap = initialize_tagger("data/lexicon.txt").unwrap();
+    let sentence = vec![
+        ("The", Wordclass::ANY),
+        ("unquick", Wordclass::JJ),
+        ("brown", Wordclass::ANY),
+        ("lazy", Wordclass::ANY),
+        ("dog", Wordclass::ANY),
+    ];
+    assert!(!delete_prefix(sentence.clone(), 1, "un", &wc_mapping));
+    assert!(!delete_prefix(sentence.clone(), 2, "aaa", &wc_mapping));
+    assert!(!delete_prefix(sentence.clone(), 2, "bro", &wc_mapping));
+
+}
+
+
+#[test]
+fn test_delete_f_prefix_found() {
+    let wc_mapping: WordclassMap = initialize_tagger("data/lexicon.txt").unwrap();
+    let sentence = vec![
+        ("The", Wordclass::DT),
+        ("unquick", Wordclass::JJ),
+        ("unbrown", Wordclass::JJ),
+        ("fox", Wordclass::NN),
+    ];
+    assert!(f_delete_prefix(sentence.clone(), 1, "un", &wc_mapping));
+    assert!(f_delete_prefix(sentence.clone(), 2, "un", &wc_mapping));
+
+}
+
+#[test]
+fn test_delete_f_prefix_not_found() {
+    let wc_mapping: WordclassMap = initialize_tagger("data/lexicon.txt").unwrap();
+    let sentence = vec![
+        ("The", Wordclass::DT),
+        ("unquick", Wordclass::ANY),
+        ("brown", Wordclass::JJ),
+        ("fox", Wordclass::NN),
+    ];
+    assert!(!f_delete_prefix(sentence.clone(), 1, "un", &wc_mapping));
+    assert!(!f_delete_prefix(sentence.clone(), 2, "zzz", &wc_mapping));
+    assert!(!f_delete_prefix(sentence.clone(), 2, "bro", &wc_mapping));
+
 }
 
 
@@ -125,7 +302,6 @@ fn test_add_f_suffix_not_found() {
     assert!(!f_add_suffix(sentence.clone(), 2, "zzz", &wc_mapping));
 
 }
-
 
 #[test]
 fn test_word_in_lexicon() {
