a6ed7c3044397c2cdc95732b4fb92b09d013bb37
Toby Clark <psytc9@nottingham.ac.uk>
2024-10-18 23:27:29 +0100

    add: basic inductive CCG datatypes (using Box)

--- src/rs_ccg.rs
+++ src/rs_ccg.rs
@@ -1,4 +1,5 @@
 
+
 /// Enumeration to over atomic CCG categories like S, NP, N, etc.
 #[derive(Debug, Clone)]
 enum Category {
@@ -7,6 +8,7 @@ enum Category {
     N,  // Noun
 }
 
+
 /// Enumeration to represent the functor application categories
 #[derive(Debug, Clone)]
 enum Functor {
@@ -14,35 +16,40 @@ enum Functor {
     Backward(Box<CCG>, Box<CCG>), // A\B (applies between any two CCG categories)
 }
 
+
 /// Enumeration to represent a category (either atomic or a functor)
 #[derive(Debug, Clone)]
 enum CCG {
-    Atom(Category),         // Atomic category
-    Func(Functor),          // Functor (applies between categories)
+    Atomic(Category),          // Atomic category
+    Functor(Functor),          // Functor (applies between categories)
 }
 
-/// Helper functions for the CCG type.
-impl CCG {
-    // Function to create forward functors
-    fn forward(left: CCG, right: CCG) -> CCG {
-        CCG::Func(Functor::Forward(Box::new(left), Box::new(right)))
-    }
 
-    // Function to create backward functors
-    fn backward(left: CCG, right: CCG) -> CCG {
-        CCG::Func(Functor::Backward(Box::new(left), Box::new(right)))
-    }
+// Function to create backward functors, as a CCG.
+fn create_forward_functor(left: CCG, right: CCG) -> CCG {
+    CCG::Functor(Functor::Forward(Box::new(left), Box::new(right)))
 }
+
+
+// Function to create forward functors, as a CCG.
+fn create_backward_functor(left: CCG, right: CCG) -> CCG {
+    CCG::Functor(Functor::Backward(Box::new(left), Box::new(right)))
+}
+
 
 // Temporary test.
 pub fn test_ccg() {
     // Example: (S\NP)\(S\NP)
-    let s = CCG::Atom(Category::S);
-    let np = CCG::Atom(Category::NP);
+    let s = CCG::Atomic(Category::S);
+    let np = CCG::Atomic(Category::NP);
 
-    let s_np = CCG::backward(s.clone(), np.clone()); // S\NP
-    let complex_category = CCG::backward(s_np.clone(), s_np.clone()); // (S\NP)\(S\NP)
+    let s_np = create_backward_functor(s.clone(), np.clone()); // S\NP
+    let s = CCG::Atomic(Category::S); // S\NP
+    let complex_category = create_forward_functor(s_np.clone(), s_np.clone()); // (S\NP)\(S\NP)
 
     // Print the complex category in a human-readable format
     println!("{:?}", complex_category);
+    println!("{:?}", s_np);
+    println!("{:?}", s);
+
 }
